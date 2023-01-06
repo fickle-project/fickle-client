8e18921386fcb1f6a040a19868dfbb8e745eff97
K-shir0 <kakishiro2190394@gmail.com>
2023-01-06 13:10:24 +0900

    add: column と issue を追加

---
+++ lib/components/_components.dart
@@ -0,0 +1,2 @@
+export 'package:fickle_client/components/fickle_column.dart';
+export 'package:fickle_client/components/fickle_issue.dart';
---
+++ lib/components/fickle_column.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import 'package:fickle_client/components/_components.dart';
+
+class FickleColumn extends StatelessWidget {
+  const FickleColumn({
+    super.key,
+    required this.name,
+    required this.color,
+    this.issues = const [],
+  });
+
+  final String name;
+  final Color color;
+  final List<FickleIssue> issues;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: [
+        Card(
+          child: ListTile(
+            title: Text(name),
+            tileColor: color,
+          ),
+        ),
+        ...issues
+      ],
+    );
+  }
+}
